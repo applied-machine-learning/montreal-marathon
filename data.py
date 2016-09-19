32f99d8adb0d63e94600b15eadf69aaba99d92e0
Kevin Li <github@mail.kevinl.io>
2016-09-19 12:52:54 -0400

    wip

---
+++ data.py
@@ -0,0 +1,21 @@
+# The point of this is to take the provided raw data 
+# and correctly parse it.
+
+# Each row will have the following columns.
+
+'''
+- participant_id 
+- gender 
+- age 
+- number_of_events_participated_in
+- number_of_marathons
+- number_of_
+'''
+
+'''
+Categories of events:
+    - 
+'''
+
+if __name__ == "__main__":
+    pass
