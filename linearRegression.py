5918b0839a122fc684dc8fa2692ff2a0ef5e9a0b
Kevin Li <github@mail.kevinl.io>
2016-09-20 21:09:06 -0400

    linear regression wip

---
+++ linearRegression.py
@@ -0,0 +1,11 @@
+import data
+
+dataset = MarathonDataset(loadCSV("raw_data/Project1_data.csv"))
+
+''' Headers Used:
+- Gender
+- Age
+- log (number of marathons / average marathon time (handle div by zero))
+- 
+'''
+matrix = 
