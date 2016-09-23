fdf0c48d79a5a7d205303231cfb1753ff23ac717
Kevin Li <github@mail.kevinl.io>
2016-09-23 11:52:24 -0400

    wip linear regression

--- linearRegression.py
+++ linearRegression.py
@@ -9,7 +9,7 @@ def run():
     - Age
     - log (number of non 2015 marathons / average non 2015 marathon time (handle div by zero))
     '''
-    headers = [Headers.gender, Headers.age, Headers.logNon2015MarathonRatio]
+    headers = [Headers.gender, Headers.age, Headers.averageNon2015FullMMTime]
 
     inputs = np.matrix(dataset.request(headers))
     inputs = np.insert(inputs, 3, 1, axis=1)
@@ -17,10 +17,17 @@ def run():
     outputs = np.matrix(dataset.request([Headers.MM2015Time]))
 
     weights = np.linalg.inv(np.transpose(inputs) * inputs) * (np.transpose(inputs) * outputs)
+
+    predictions = inputs * weights
+
+    err = outputs - predictions
+
+    sse = np.transpose(err) * err
     
-    print inputs
-    print outputs
     print weights
+    print predictions
+    print outputs
+    print sse
 
 if __name__ == "__main__":
     run()
